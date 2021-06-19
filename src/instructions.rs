1e98bc011cbbc1d509853eaf142867720fbfc863
Felix Andreas <felix.andreas95@googlemail.com>
2021-06-19 23:55:45 +0200

    Decode full RV32I Base Instruction Set

--- src/instructions.rs
+++ src/instructions.rs
@@ -13,63 +13,65 @@ use crate::formats::*;
 
 #[derive(Debug)]
 pub enum Instruction {
-    // // LUI 0110111
-    // LUI(UType),
+    // LUI 0110111
+    LUI(UType),
 
-    // // AUPIC 0010111
-    // AUIPC(UType),
+    // AUPIC 0010111
+    AUIPC(UType),
 
     // JAL 1101111
     JAL(JType),
+
     // JALR 1100111
-    // JALR(IType),
-    // // BRANCH 1100011
-    // BEQ(BType),
-    // BNE(BType),
-    // BLT(BType),
-    // BGE(BType),
-    // BLTU(BType),
-    // BGEU(BType),
+    JALR(IType),
 
-    // // LOAD 0000011
-    // LB(IType),
-    // LH(IType),
-    // LW(IType),
-    // LBU(IType),
-    // LHU(IType),
+    // BRANCH 1100011
+    BEQ(BType),
+    BNE(BType),
+    BLT(BType),
+    BGE(BType),
+    BLTU(BType),
+    BGEU(BType),
 
-    // // STORE 0100011
-    // SB(SType),
-    // SH(SType),
-    // SW(SType),
+    // LOAD 0000011
+    LB(IType),
+    LH(IType),
+    LW(IType),
+    LBU(IType),
+    LHU(IType),
 
-    // // OP-IMM 0010011
-    // ADDI(IType),
-    // SLTI(IType),
-    // SLTIU(IType),
-    // XORI(IType),
-    // ORI(IType),
-    // ANDI(IType),
-    // SLLI(IType),
-    // SRLI(IType),
-    // SRAI(IType),
+    // STORE 0100011
+    SB(SType),
+    SH(SType),
+    SW(SType),
 
-    // // OP 0110011
-    // ADD(RType),
-    // SUB(RType),
-    // SLL(RType),
-    // SLT(RType),
-    // SLTU(RType),
-    // XOR(RType),
-    // SRL(RType),
-    // SRA(RType),
-    // OR(RType),
-    // AND(RType),
+    // OP-IMM 0010011
+    ADDI(IType),
+    SLTI(IType),
+    SLTIU(IType),
+    XORI(IType),
+    ORI(IType),
+    ANDI(IType),
+    SLLI(IType),
+    SRLI(IType),
+    SRAI(IType),
 
-    // // FENCE 0001111
-    // FENCE,
+    // OP 0110011
+    ADD(RType),
+    SUB(RType),
+    SLL(RType),
+    SLT(RType),
+    SLTU(RType),
+    XOR(RType),
+    SRL(RType),
+    SRA(RType),
+    OR(RType),
+    AND(RType),
 
-    // // SYSTEM 1110011
-    // ECALL,
-    // EBREAK,
+    // FENCE 0001111
+    FENCE,
+
+    // SYSTEM 1110011
+    ECALL,
+    EBREAK,
 }
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ use xmas_elf::program;
 mod formats;
 mod instructions;
 
-// use formats::{BType, IType, JType, RType, SType, UType};
-use formats::JType;
+use formats::{BType, IType, JType, RType, SType, UType};
 use instructions::Instruction;
 
 const PC: usize = 32;
@@ -54,48 +53,89 @@ fn dump(registers: &Register) {
 }
 
 fn decode(code: u32) -> Instruction {
-    let opcode = code & 0x7f;
+    let opcode = code & 0b111_1111;
+    let funct3 = code >> 12 & 0b111;
+    let funct7 = code >> 25 & 0b111_1111;
     match opcode {
         // LUI
-        0b0110111 => {
-            unimplemented!("LUI")
-        }
+        0b0110111 => Instruction::LUI(UType(code)),
         // AUIPC
-        0b0010111 => {
-            unimplemented!("AUIPC");
-        }
+        0b0010111 => Instruction::AUIPC(UType(code)),
         // JAL
-        0b1101111 => {
-            println!("JAL");
-            Instruction::JAL(JType(code))
-        }
+        0b1101111 => Instruction::JAL(JType(code)),
         // JALR
-        0b1100111 => {
-            unimplemented!("JALR");
-        }
+        0b1100111 => Instruction::JALR(IType(code)),
         // BRANCH
-        0b1100011 => {
-            unimplemented!("BRANCH");
-        }
+        0b1100011 => match funct3 {
+            0b000 => Instruction::BEQ(BType(code)),
+            0b001 => Instruction::BNE(BType(code)),
+            0b100 => Instruction::BLT(BType(code)),
+            0b101 => Instruction::BGE(BType(code)),
+            0b110 => Instruction::BLTU(BType(code)),
+            0b111 => Instruction::BGEU(BType(code)),
+            _ => unreachable!(),
+        },
         // LOAD
-        0b0000011 => {
-            unimplemented!("LOAD");
-        }
+        0b0000011 => match funct3 {
+            0b000 => Instruction::LB(IType(code)),
+            0b001 => Instruction::LH(IType(code)),
+            0b010 => Instruction::LW(IType(code)),
+            0b100 => Instruction::LBU(IType(code)),
+            0b101 => Instruction::LHU(IType(code)),
+            _ => unreachable!(),
+        },
         // STORE
-        0b0100011 => {
-            unimplemented!("STORE");
-        }
-        // IMM
-        0b0010011 => {
-            unimplemented!("OP-IMM");
-        }
+        0b0100011 => match funct3 {
+            0b000 => Instruction::SB(SType(code)),
+            0b001 => Instruction::SH(SType(code)),
+            0b010 => Instruction::SW(SType(code)),
+            _ => unreachable!(),
+        },
+        // OP-IMM
+        0b0010011 => match funct3 {
+            0b000 => Instruction::ADDI(IType(code)),
+            0b010 => Instruction::SLTI(IType(code)),
+            0b011 => Instruction::SLTIU(IType(code)),
+            0b100 => Instruction::XORI(IType(code)),
+            0b110 => Instruction::ORI(IType(code)),
+            0b111 => Instruction::ANDI(IType(code)),
+            0b001 => Instruction::SLLI(IType(code)),
+            0b101 => match funct7 {
+                0b0000000 => Instruction::SRLI(IType(code)),
+                0b0100000 => Instruction::SRAI(IType(code)),
+                _ => unreachable!(),
+            },
+            _ => unreachable!(),
+        },
         // OP
-        0b0110011 => {
-            unimplemented!("OP");
-        }
-        _ => {
-            panic!("This instruction is not implemented yet, and I don't know what to do bye bye!");
-        }
+        0b0110011 => match funct3 {
+            0b000 => match funct7 {
+                0b0000000 => Instruction::ADD(RType(code)),
+                0b0100000 => Instruction::SUB(RType(code)),
+                _ => unreachable!(),
+            },
+            0b001 => Instruction::SLL(RType(code)),
+            0b010 => Instruction::SLT(RType(code)),
+            0b011 => Instruction::SLTU(RType(code)),
+            0b100 => Instruction::XOR(RType(code)),
+            0b101 => match funct7 {
+                0b0000000 => Instruction::SRL(RType(code)),
+                0b0100000 => Instruction::SRA(RType(code)),
+                _ => unreachable!(),
+            },
+            0b110 => Instruction::OR(RType(code)),
+            0b111 => Instruction::AND(RType(code)),
+            _ => unreachable!(),
+        },
+        // FENCE
+        0b0001111 => Instruction::FENCE,
+        // SYSTEM
+        0b1110011 => match code >> 20 & 1 {
+            0 => Instruction::ECALL,
+            1 => Instruction::EBREAK,
+            _ => unreachable!(),
+        },
+        _ => panic!("Don't know how to decode this :("),
     }
 }
 
@@ -104,15 +144,18 @@ fn step(registers: &mut Register, memory: &mut Memory) {
     let code = read_u32(&memory, registers[PC]);
     let instruction = decode(code);
 
-    print!("instruction: {:?}", instruction);
-
     // Execute
     match instruction {
         Instruction::JAL(j_type) => {
-            let rd = j_type.rd();
             let imm = j_type.imm();
+            let rd = j_type.rd();
             registers[PC] += imm;
             registers[rd as usize] = registers[PC] + 4;
+            println!("JAL imm: 0x{:x} rd: 0x{:x}", imm, rd);
+        }
+        _ => {
+            println!("instruction: {:?}", instruction);
+            panic!("This instruction is not implemented yet, and I don't know what to do bye bye!")
         }
     }
 
